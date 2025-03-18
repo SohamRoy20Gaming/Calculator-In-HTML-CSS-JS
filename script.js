228c4eb9c7154ef3d65ef06fbe80c684d4fb9f4b
SohamRoy20Gaming <109818645+SohamRoy20Gaming@users.noreply.github.com>
2025-03-18 14:21:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+    document.getElementById("display").value += value;
 }
 
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    document.getElementById("display").value = "";
 }
 
 function deleteLast() {
-    let display = document.getElementById('display');
+    let display = document.getElementById("display");
     display.value = display.value.slice(0, -1);
 }
 
-function calculate() {
-    let display = document.getElementById('display');
+function calculateResult() {
+    let display = document.getElementById("display");
     try {
         display.value = eval(display.value);
-    } catch (e) {
-        display.value = 'Error';
+    } catch {
+        display.value = "Error";
     }
 }
